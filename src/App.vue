3e84e1577fd22519e363b4f933b36b9850657b92
Arthur Hain <arthur.hain.2020@scis.smu.edu.sg>
2022-11-29 22:51:08 +0800

    changed font family

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,12 @@ export default {
 <style>
 #app {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
-  font-family: 'Montserrat', sans-serif;
+  /* font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale; */
   text-align: center;
   color: #2c3e50;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
 }
 
 @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
