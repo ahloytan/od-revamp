62368c1b4e5f7b2fe119983d2591d5c6d7186ab9
Arthur Hain <arthur.hain.2020@scis.smu.edu.sg>
2022-12-08 22:58:33 +0800

    added submit form logic

--- src/views/contact-us.vue
+++ src/views/contact-us.vue
@@ -6,27 +6,28 @@
 
             <div class="form-input" id="firstName">
                 <label for="first-name" class="form-label">First Name*</label>
-                <input type="text" class="form-control" id="first-name">
+                <input v-model="userInput.firstName" type="text" class="form-control" id="first-name">
             </div>
 
             <div class="form-input" id="lastName">
                 <label for="last-name" class="form-label">Last Name*</label>
-                <input type="text" class="form-control" id="last-name">
+                <input v-model="userInput.lastName" type="text" class="form-control" id="last-name">
             </div>
 
             <div class="form-input" id="email">
                 <label for="email-input" class="form-label">Email*</label>
-                <input type="text" class="form-control" id="email-input">
+                <input v-model="userInput.email" type="text" class="form-control" id="email-input">
             </div>
 
             <div class="form-input" id="interest">
                 <label for="checkboxes" class="form-label">Interests</label>
 
-                <div v-for="interest in interests" :key="interest" id="checkboxes">
+                <div v-for="(key, interest) in interests" :key="interest" id="checkboxes">
                     <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
+                    <input v-model="interests[interest]" class="form-check-input" type="checkbox" value="" id="{{interest}}">
+                    <label class="form-check-label" for="{{interest}}">
                         {{interest}}
+                        {{key}}
                     </label>
                     </div>
                 </div>
@@ -35,11 +36,11 @@
 
             <div class="form-input" id="comments">
                     <label for="comments-input" class="form-label">Comments | Feedback | Suggestions</label>
-                    <textarea class="form-control" id="comments-input" rows="3"></textarea>
+                    <textarea v-model="userInput.comments" class="form-control" id="comments-input" rows="3"></textarea>
             </div>
 
             <div class="form-input" id="comments">
-                <button type="button" class="btn btn-primary">Submit</button>
+                <button @click="handleSubmit(interests, userInput)" type="button" class="btn btn-primary">Submit</button>
             </div>
 
 
@@ -56,19 +57,28 @@ export default {
     data(){
         return {
             title: "Contact Us",
-            interests: [
-                "Coaching",
-                "Social",
-                "Education", 
-                "Partnership"
-            ]
+            interests: {
+                "Coaching": false,
+                "Social": false,
+                "Education": false, 
+                "Partnership": false
+            }, 
+            userInput: {
+                firstName: "Arthur", 
+                lastName: "Hain", 
+                email: "arthurhain@gmail.com", 
+                comments: "Hello there kenobi.", 
+            }
         }
     }, 
     mounted() {
 
     }, 
     methods: {
-
+        handleSubmit(interests, userInput) {
+            console.log(interests)
+            console.log(userInput)
+        }
     }
 }
 </script>
